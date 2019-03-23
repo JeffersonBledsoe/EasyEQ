cb5144c6295f4959c3d1529ae8c0b03e9877d14e
Jefferson Bledsoe <me@jeffersonbledsoe.com>
2019-03-23 16:06:09 +0000

    Update ProjucerMac script

--- Scripts/ProjucerGui.sh
+++
@@ -1,4 +0,0 @@
-pushd ../JUCE/extras/Projucer/Builds/MacOSX
-xcodebuild -configuration "Release" GCC_PREPROCESSOR_DEFINITIONS='$(inherited) JUCER_ENABLE_GPL_MODE'
-popd
-exec ../JUCE/extras/Projucer/Builds/MacOSX/build/Release/Projucer.app/Contents/MacOS/Projucer ./*.jucer & exit
